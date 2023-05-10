f8fb3990052771409e311fbd4383b8f21e7940fa
Juan Mattessich <mattessich84@gmail.com>
2023-05-09 22:12:31 -0300

    ultimos cambios y retoques

--- stock.js
+++ stock.js
@@ -1,6 +1,3 @@
-// Array para almacenar los productos del inventario
-let inventario = [];
-
 // Referencias a los elementos HTML relevantes
 const tablaInventario = document.querySelector('#inventario table tbody');
 const btnAgregarProducto = document.querySelector('#btnAgregarProducto');
@@ -11,13 +8,16 @@ const inputNombreProducto = document.querySelector('#nombreProducto');
 const inputCantidadProducto = document.querySelector('#cantidadProducto');
 const inputPrecioProducto = document.querySelector('#precioProducto');
 
+// Array para almacenar los productos del inventario
+let inventario = [];
+
 // Función para agregar un producto al inventario
 function agregarProducto(nombre, cantidad, precio) {
   // Crea un objeto producto con las propiedades dadas
   const producto = {
-    nombre: nombre,
-    cantidad: cantidad,
-    precio: precio
+    nombre,
+    cantidad,
+    precio
   };
 
   // Agrega el producto al inventario
@@ -30,75 +30,21 @@ function mostrarInventario() {
   tablaInventario.innerHTML = '';
 
   // Agrega cada producto del inventario a la tabla
-  inventario.forEach(function(producto) {
+  inventario.forEach((producto, index) => {
     const fila = document.createElement('tr');
     fila.innerHTML = `
       <td>${producto.nombre}</td>
       <td>${producto.cantidad}</td>
       <td>${producto.precio.toFixed(2)}</td>
       <td>
-        <button class="btn-editar">Editar</button>
-        <button class="btn-eliminar">Eliminar</button>
+        <button onclick="editarProducto(${index})">Editar</button>
+        <button onclick="eliminarProducto(${index})">Eliminar</button>
       </td>
     `;
     tablaInventario.appendChild(fila);
   });
-
-  // Agrega los event listeners para los botones de editar y eliminar
-  const btnEditar = document.querySelectorAll('.btn-editar');
-  btnEditar.forEach(function(btn) {
-    btn.addEventListener('click', function() {
-      // TODO: Agregar lógica para editar el producto
-      console.log('Editar producto');
-    });
-  });
-
-  const btnEliminar = document.querySelectorAll('.btn-eliminar');
-  btnEliminar.forEach(function(btn) {
-    btn.addEventListener('click', function() {
-      // TODO: Agregar lógica para eliminar el producto
-      console.log('Eliminar producto');
-    });
-  });
 }
 
-// Agregar un producto cuando el usuario envía el formulario
-formAgregarProducto.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que el formulario se envíe automáticamente
-
-  // Obtiene los valores del formulario
-  const nombre = inputNombreProducto.value;
-  const cantidad = parseInt(inputCantidadProducto.value);
-  const precio = parseFloat(inputPrecioProducto.value);
-
-  // Agrega el producto al inventario
-  agregarProducto(nombre, cantidad, precio);
-
-  // Actualiza la tabla del inventario
-  mostrarInventario();
-
-  // Cierra el modal
-  modalAgregarProducto.style.display = 'none';
-});
-
-// Mostrar el modal para agregar un producto cuando el usuario hace clic en el botón correspondiente
-btnAgregarProducto.addEventListener('click', function() {
-  modalAgregarProducto.style.display = 'block';
-});
-
-// Cerrar el modal cuando el usuario hace clic en el botón de cerrar o fuera del modal
-btnCerrarModal.addEventListener('click', function() {
-  modalAgregarProducto.style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-  if (event.target == modalAgregarProducto) {
-    modalAgregarProducto.style.display = 'none';
-  }
-});
-
-// Inicializa la tabla del inventario al cargar la página
-mostrarInventario();
 // Función para editar un producto del inventario
 function editarProducto(index) {
   // Obtiene el producto correspondiente al índice dado
@@ -128,36 +74,31 @@ function eliminarProducto(index) {
   mostrarInventario();
 }
 
-// Función para agregar un producto al inventario
-function agregarProducto(nombre, cantidad, precio) {
-  // Crea un objeto producto con las propiedades dadas
-  const producto = {
-    nombre: nombre,
-    cantidad: cantidad,
-    precio: precio
-  };
+// Agregar un producto cuando el usuario envía el formulario
+formAgregarProducto.addEventListener('submit', (event) => {
+  event.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+  // Obtiene los valores del formulario
+  const nombre = inputNombreProducto.value;
+  const cantidad = parseInt(inputCantidadProducto.value);
+  const precio = parseFloat(inputPrecioProducto.value);
 
   // Agrega el producto al inventario
-  inventario.push(producto);
-}
+  agregarProducto(nombre, cantidad, precio);
 
-// Función para mostrar los productos del inventario en la tabla
-function mostrarInventario() {
-  // Limpia la tabla
-  tablaInventario.innerHTML = '';
+  // Actualiza la tabla del inventario
+  mostrarInventario();
 
-  // Agrega cada producto del inventario a la tabla
-  inventario.forEach(function(producto, index) {
-    const fila = document.createElement('tr');
-    fila.innerHTML = `
-      <td>${producto.nombre}</td>
-      <td>${producto.cantidad}</td>
-      <td>${producto.precio.toFixed(2)}</td>
-      <td>
-        <button onclick="editarProducto(${index})">Editar</button>
-        <button onclick="eliminarProducto(${index})">Eliminar</button>
-      </td>
-    `;
-    tablaInventario.appendChild(fila);
-  });
-}
+  // Cierra el modal
+  modalAgregarProducto.style.display = 'none';
+});
+
+// Mostrar el modal para agregar un producto cuando el usuario hace clic en el botón correspondiente
+btnAgregarProducto.addEventListener('click', () => {
+  modalAgregarProducto.style.display = 'block';
+});
+
+// Cerrar el modal cuando el usuario hace clic en el botón de cerrar o fuera del modal
+btnCerrarModal.addEventListener('click', () => {
+  modalAgregarProducto.style.display = 'none';
+});
